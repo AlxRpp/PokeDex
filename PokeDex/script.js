fefd1c0ca4f21991838696694ce39672300654cb
AlxRpp <alexanderr26@outlook.de>
2024-11-07 11:39:14 +0100

    second commite

--- PokeDex/script.js
+++ PokeDex/script.js
@@ -1,55 +1,365 @@
 const baseUrl = "https://pokeapi.co/api/v2/";
 const limitUrl = "pokemon?limit=";
-let limit = 20;
 const offsetUrl = "&offset=";
+let limit = 20;
 let offset = 0;
-
 let allPoke = [];
-let currentPoke = allPoke;
+let favoredPoke = [];
+let isInFavoritesView = false;
 
 
- async function init() {
-   await loadAllPoke();
+
+
+async function init() {
+    currentPoke = allPoke;
+    await loadAllPoke();
     renderPoke();
+    showButton();
+    loadFromLocalStorage();
+    amountFav();
 }
 
 
 async function loadAllPoke() {
-    let response = await fetch(baseUrl + limitUrl + limit + offsetUrl + offset);
-    let responseAsJson = await response.json()
-    // console.log(responseAsJson);
-    
-    for (let index = 0; index < responseAsJson.results.length; index++) {
-        const pokeName = responseAsJson.results[index].name;
-        const pokeUrl = responseAsJson.results[index].url;
-        let detailResponse = await fetch(pokeUrl);
-        let pokeDetails = await detailResponse.json();
-        let pokeImage = pokeDetails.sprites.other.dream_world.front_default;
-        let pokeType = pokeDetails.types.map(typeInfo => typeInfo.type.name)
-        let pokeId = pokeDetails.id
-        allPoke.push(
-            {
-                "Name":pokeName,
-                "URL":pokeUrl,
-                "Id":pokeId,
-                "Image":pokeImage,
-                "Type":pokeType
-            }
-        )
-        console.log(pokeName, pokeUrl);
+    showLoadingSpinner();
+    try {
+        let response = await fetch(baseUrl + limitUrl + limit + offsetUrl + offset);
+        let responseAsJson = await response.json()
+        for (let index = 0; index < responseAsJson.results.length; index++) {
+            const pokeName = responseAsJson.results[index].name;
+            const pokeUrl = responseAsJson.results[index].url;
+            let detailResponse = await fetch(pokeUrl);
+            let pokeDetails = await detailResponse.json();
+            let pokeImage = pokeDetails.sprites.other['official-artwork'].front_default;
+            let pokeGif = pokeDetails.sprites.other.showdown.front_default;
+            let pokeType = pokeDetails.types.map(typeInfo => typeInfo.type.name).join(' ');
+            let pokeId = pokeDetails.id
+            pushPokeToArr(pokeName, pokeUrl, pokeId, pokeImage, pokeType, pokeGif)
+            //console.log(pokeName, pokeUrl);
+            fetchSinglePoke(pokeUrl);
+        }
+
     }
+    catch (error) {
+        console.error("das lief etwas schief");
+    }
+
+    offset = allPoke.length;
+    removeLoadingSpinner();
+
+
 }
 
-function renderPoke(){
-    let content = document.getElementById('content');
-    content.innerHTML = "";
 
+function pushPokeToArr(pokeName, pokeUrl, pokeId, pokeImage, pokeType, pokeGif) {
+    allPoke.push(
+        {
+            "Liked": false,
+            "Name": pokeName,
+            "URL": pokeUrl,
+            "Id": pokeId,
+            "Image": pokeImage,
+            "Type": pokeType,
+            "Gif": pokeGif
+        }
+    )
+}
+
+function renderPoke() {
+    isInFavoritesView = false;
+    let content = document.getElementById('content');
+    let html = "";
+    content.innerHTML = "";
     for (let pokeIndex = 0; pokeIndex < allPoke.length; pokeIndex++) {
         const pokemon = allPoke[pokeIndex];
-        const id = pokeIndex +1
-         content.innerHTML += pokeCard(pokemon, id);
+        html += pokeCard(pokemon);
+    }
+    content.innerHTML += html;
+    showButton();
+}
 
+
+async function showNextPoke() {
+    closeSingleCard();
+    document.getElementById('morePoke').disabled = true;
+    await loadAllPoke();
+    renderPoke();
+    document.getElementById('morePoke').disabled = false;
+}
+
+
+function showLoadingSpinner() {
+    document.getElementById('loadingSpinner').classList.remove('d-none');
+}
+
+
+function removeLoadingSpinner() {
+    document.getElementById('loadingSpinner').classList.add('d-none');
+}
+
+
+function showButton() {
+    let myPoke = currentPoke.length;
+    if (myPoke > 0) {
+        document.getElementById('morePoke').classList.remove('d-none');
     }
 }
 
-  
+
+function filterPoke() {
+    let searchField = document.getElementById('search').value.toLowerCase();
+    if (searchField.length > 2) {
+        let filteredPoke = allPoke.filter(poke => poke.Name.toLowerCase().includes(searchField));
+        closeSingleCard();
+        document.getElementById('morePoke').classList.add('d-none');
+
+
+        renderFilteredPoke(filteredPoke);
+    } else {
+        renderFilteredPoke(allPoke);
+    }
+}
+
+
+function renderFilteredPoke(filteredPoke) {
+    let content = document.getElementById('content');
+    let html = "";
+    content.innerHTML = "";
+    filteredPoke.forEach(pokemon => {
+        html += pokeCard(pokemon)
+    });
+    content.innerHTML = html;
+}
+
+
+
+async function fetchSinglePoke(url) {
+    try {
+        let singlePoke = await fetch(url);
+        let singlePokeAsJson = await singlePoke.json();
+        let speciesURL = singlePokeAsJson.species.url;
+        let speciesData = await fetch(speciesURL);
+        let speciesDataAsJson = await speciesData.json()
+        let genderRate = speciesDataAsJson.gender_rate
+        let female = (genderRate / 8) * 100
+        let male = 100 - female
+        let evolutionUrl = speciesDataAsJson.evolution_chain.url;
+        let evolution = await fetch(evolutionUrl);
+        let evolutionData = await evolution.json();
+        updateArray(speciesDataAsJson, singlePokeAsJson, male, female, evolutionData);
+    } catch (error) {
+        console.error(error, "das hat nicht geklappt");
+    }
+}
+
+
+function updateArray(speciesDataAsJson, singlePokeAsJson, male, female, evolutionData) {
+    let newData = {
+        "About": {
+            "Species": speciesDataAsJson.genera[7].genus.split(' ')[0],
+            "Height": singlePokeAsJson.height,
+            "Weight": singlePokeAsJson.weight,
+            "Abilities": singlePokeAsJson.abilities.map(abilitie => abilitie.ability.name).join(' / '),
+        },
+
+        "Breeding": {
+            "Male": male,
+            "Female": female,
+            "Growth_Rate": speciesDataAsJson.growth_rate.name,
+            "Egg_Groups": speciesDataAsJson.egg_groups[0].name,
+        },
+
+        "Base Stats": {
+            "name": singlePokeAsJson['stats'].map(data => data.stat.name),
+            "stats": singlePokeAsJson['stats'].map(data => data.base_stat)
+        },
+
+        "Moves": singlePokeAsJson['moves'].slice(0, 10).map(move => move.move.name).join(' '),
+        "Evolution_Chain": evolutionData
+    }
+    let pokeUpdate = allPoke.find(pokemon => pokemon.Id === singlePokeAsJson.id);
+    if (pokeUpdate) {
+        Object.assign(pokeUpdate, newData);
+    }
+}
+
+
+function renderSinglePoke(id){
+    let pokemon = isInFavoritesView
+    ? favoredPoke.find(poke => poke.Id === id)
+    :allPoke.find(poke => poke.Id === id);
+
+    if (pokemon){
+        let content = document.getElementById('singleCard');
+        content.innerHTML = openSinglePoke(pokemon);
+    }
+    
+}
+
+// function nextFavoredPoke(currentId){
+//     let currentIndex = favoredPoke.findIndex(poke => poke.Id === currentId);
+//     let nextIndex = currentIndex++;
+//     if (nextIndex >= favoredPoke.length) {
+//         nextIndex = 0;
+//     }
+//     renderSinglePoke(favoredPoke[nextIndex].Id);
+// }
+
+// function previousFavoredPoke (currentId){
+//     let currentIndex = favoredPoke.findIndex(poke => poke.Id === currentId);
+//     let prevIndex = currentIndex--;
+//     if (prevIndex < 0) {
+//         prevIndex = favoredPoke.length -1;
+//     }
+//     renderSinglePoke(favoredPoke[prevIndex].Id)
+// }
+
+
+// function renderSinglePoke(id) {
+//     let pokemon = allPoke.find(poke => poke.Id === id);
+//     if (pokemon) {
+//         let content = document.getElementById('singleCard');
+//         content.innerHTML = openSinglePoke(pokemon);
+//     }
+// }
+
+function closeSingleCard() {
+    document.getElementById('singleCard').innerHTML = ""
+    let backGround = document.getElementById('singleCard').classList.add('d-none');
+    enableScrolling();
+}
+
+
+function nextPoke(currentId) {
+    let currentIndex = isInFavoritesView
+        ? favoredPoke.findIndex(poke => poke.Id === currentId)
+        : allPoke.findIndex(poke => poke.Id === currentId);
+
+    let nextIndex = currentIndex + 1;
+
+    // Wenn wir am Ende der Liste sind, zurück zum Anfang
+    if (isInFavoritesView) {
+        if (nextIndex >= favoredPoke.length) {
+            nextIndex = 0;
+        }
+        renderSinglePoke(favoredPoke[nextIndex].Id);
+    } else {
+        if (nextIndex >= allPoke.length) {
+            nextIndex = 0;
+        }
+        renderSinglePoke(allPoke[nextIndex].Id);
+    }
+}
+
+
+function previousPoke(currentId) {
+    let currentIndex = isInFavoritesView
+        ? favoredPoke.findIndex(poke => poke.Id === currentId)
+        : allPoke.findIndex(poke => poke.Id === currentId);
+
+    let prevIndex = currentIndex - 1;
+
+    // Wenn wir am Anfang der Liste sind, zum Ende springen
+    if (isInFavoritesView) {
+        if (prevIndex < 0) {
+            prevIndex = favoredPoke.length - 1;
+        }
+        renderSinglePoke(favoredPoke[prevIndex].Id);
+    } else {
+        if (prevIndex < 0) {
+            prevIndex = allPoke.length - 1;
+        }
+        renderSinglePoke(allPoke[prevIndex].Id);
+    }
+}
+
+
+function openSettings() {
+    document.getElementById('settings').classList.toggle('settings-closed')
+}
+
+
+function changeSettings() {
+    let newsetting = document.getElementById('setting').value;
+    newsetting = +newsetting;
+    if (newsetting >= 1 && newsetting <= 100) {
+        limit = newsetting;
+        openSettings();
+        document.getElementById('setting').value = "";
+        showNextPoke();
+    }
+    else {
+        alert("Please enter a number between 1 and 100")
+    }
+}
+
+
+function saveFavoritPoke(ID) {
+    let favoritPoke = [...allPoke, ...favoredPoke].find(poke => poke.Id === ID);
+    if (favoritPoke) {
+        if (favoritPoke.Liked) {
+            if (!favoredPoke.some(poke => poke.Id === favoritPoke.ID)) {
+                favoredPoke.push(favoritPoke);
+                console.log("Pokemon zu den Favoriten hinzugefügt");
+            }
+        } else {
+            favoredPoke = favoredPoke.filter(poke => poke.Id !== favoritPoke.Id);
+            console.log("Pokemon aus der Favoriten-Liste entfernt");
+        }
+    }
+    saveToLocalStorage();
+    amountFav();
+}
+
+
+function renderFavoreds() {
+    isInFavoritesView = true;
+    if (favoredPoke.length > 0) {
+        let content = document.getElementById('content');
+        let html = "";
+        content.innerHTML = "";
+        favoredPoke.forEach(pokemon => {
+            html += pokeCard(pokemon)
+        });
+        content.innerHTML = html;
+        closeSingleCard();
+        document.getElementById('morePoke').classList.add('d-none');
+    } else {
+        renderPoke();
+    }
+    closeSingleCard();
+}
+
+
+function amountFav() {
+    loadFromLocalStorage()
+    let amount = document.getElementById('amountFav');
+    if (favoredPoke.length > 0) {
+        amount.innerText = favoredPoke.length;
+    } else {
+        amount.innerText = "";
+    }
+}
+
+
+function saveToLocalStorage() {
+    let favoredsAsText = JSON.stringify(favoredPoke);
+    localStorage.setItem('pokemon', favoredsAsText);
+}
+
+
+function loadFromLocalStorage() {
+    let favoredsAsText = localStorage.getItem('pokemon')
+    if (favoredsAsText) {
+        favoredPoke = JSON.parse(favoredsAsText);
+
+        // let exitsInAllPoke = allPoke.some(pokemon => pokemon.Id === favoredPoke.Id);
+        // if (!exitsInAllPoke) {
+        //     allPoke.push(favoredPoke);
+        // }
+
+        allPoke.forEach(pokemon => {
+            pokemon.Liked = favoredPoke.some(favPoke => favPoke.Id === pokemon.Id)
+        });
+    }
+}
--- PokeDex/scripts/HTMLTemplates.js
+++ PokeDex/scripts/HTMLTemplates.js
@@ -1,18 +1,384 @@
-function pokeCard(pokeIndex, id){
+// MutationObserver einrichten
+
+
+function pokeCard(pokeIndex) {
+    let firstType = pokeIndex.Type.split(' ')[0];
+    let secondType = pokeIndex.Type.split(' ')[1];
+    let firstIcon = `<img src="./assests/icons/${firstType}.svg">`;
+    let secondIcon = secondType ? `<img src="./assests/icons/${secondType}.svg">` : '';
     return `
-    <div class="Poke-Card " id="${id}">
+    <div class="Poke-Card" id="${pokeIndex.Id}" onclick="renderSinglePoke(${pokeIndex.Id})">
             <div class="Card-Header">
-                <p>#${pokeIndex.Id}</p>
-                <p>${pokeIndex.Name}</p>
+                <p class="title"> <b> ${pokeIndex.Name}</b><br> 
+                #${showCorrectId(pokeIndex.Id)}</p>
+                <span></span>
             </div>
             <div class="Card-IMG ${pokeIndex.Type}">
-                <img src="${pokeIndex.Image}">
+                 <img src="${pokeIndex.Image}">
             </div>
             <div class="Card-Details">
-                <p>
-                    type(s): ${pokeIndex.Type}
-                </p>
+                <h4>Type(s):</h4>
+                <div class="Icon-Section">
+                    ${firstIcon} <span class="Type">${firstType}</span>
+                </div>
+                <div class="Icon-Section">
+                    ${secondIcon} <span class="Type">${secondType || ""}</span> 
+                </div>
             </div>
         </div>
     `;
 }
+
+function changeHeart(pokemon) {
+    if (!pokemon.Liked) {
+        return `
+        ./assests/heart.png
+        `;
+    } else {
+        return `
+        ./assests/heartWhite.png
+        `;
+    }
+}
+
+function toggleHeart(id) {
+    let pokemon = [...allPoke, ...favoredPoke].find(poke => poke.Id === id);
+
+    if (pokemon) {
+        pokemon.Liked = !pokemon.Liked;
+        saveFavoritPoke(pokemon.Id);
+        const renderedCard = openSinglePoke(pokemon);
+        document.getElementById("bigPoke").innerHTML = renderedCard;
+    }
+    if (isInFavoritesView) {
+        renderFavoreds();
+    }
+
+    if (favoredPoke.length <= 0) {
+        renderPoke()
+    }
+
+    amountFav();
+}
+
+function stopScrolling() {
+    document.getElementById('body').classList.add('of-hidden');
+}
+
+function enableScrolling() {
+    document.getElementById('body').classList.remove('of-hidden');
+}
+
+function openSinglePoke(pokemon) {
+    document.getElementById('singleCard').classList.remove('d-none');
+    let firstType = pokemon.Type.split(' ')[0];
+    let secondType = pokemon.Type.split(' ')[1];
+    let baseStatsNames = pokemon["Base Stats"].name;
+    let baseStatsValues = pokemon["Base Stats"].stats;
+    setTimeout(() => { getActive() }, 1000)
+    stopScrolling();
+
+    return `
+    <div onclick="event.stopPropagation() " class="Single-Poke-Card ${pokemon.Type}" id="bigPoke">
+            <div class="Head-Icon">
+                <div onclick="closeSingleCard()"><img src="./assests/arrowLeft.png"></div>
+                <div onclick="toggleHeart(${pokemon.Id})"><img src="${changeHeart(pokemon)}" alt="" srcset=""></div>
+            </div>
+            <div class="Head-Infos">
+                <div class="Head-Left">
+                    <h1 class="title">${pokemon.Name}</h1>
+                        <div class="Types">
+                            <p class="Type-Border title">${firstType}</p>
+                            <p class="Type-Border title">${secondType || ""}</p>
+                        </div>
+                </div>
+                <div class="Head-Right">
+                    <span>#${showCorrectId(pokemon.Id)}</span>
+                </div>
+            </div>
+            <div class="Poke-Img">
+                <img src="${pokemon.Image}">
+            </div>
+            <div>
+            <img onclick="nextPoke(${pokemon.Id})" class="doubleArrow" src="./assests/doubleArrow.png">
+            <img onclick="previousPoke(${pokemon.Id})" class="doubleArrowLeft" src="./assests/doubleArrow.png">
+
+            </div>
+            <div class="More-Information">
+                <div class="Categorys">
+                    <span class="active" onclick="renderSinglePoke(${pokemon.Id})">About</span>
+                    <span onclick='renderPolarChart(${JSON.stringify(baseStatsNames)}, ${JSON.stringify(baseStatsValues)})'>Base Stats</span>
+                    <span onclick="insertMoves(${pokemon.Id})">Moves</span>
+                    <span onclick="getEvolution(${pokemon.Id})">Evolution</span>
+                </div>
+                <div class="divider"></div>
+                <div class="About" id="insertData">
+                    
+                    <div class="Pokemon-Info">
+                        <span class="Info-Left">Species</span>
+                        <span>${pokemon.About.Species}</span>
+                    </div>
+
+                    <div class="Pokemon-Info">
+                        <span class="Info-Left">Abilities</span>
+                        <span class="title">${pokemon.About.Abilities}</span>
+                    </div>
+                        
+                    <div class="Pokemon-Info">
+                        <span class="Info-Left">Height</span>
+                        <span>${calculateHeight(pokemon)}</span>
+                    </div>
+                        
+                    <div class="Pokemon-Info">
+                        <span class="Info-Left">Weight</span>
+                        <span>${calculateWeight(pokemon)}</span>
+                    </div>  
+
+                    <div class="Pokemon-Info">
+                        <p class="Bree">Breeding</p>
+                    </div>
+
+                    <div class="Pokemon-Info">
+                        <span class="Info-Left">Gender</span>
+                          <div class="gender">
+                            <img src="./assests/male.png" alt=""> ${pokemon.Breeding.Male} %
+                            <img src="./assests/female.png" alt=""> ${pokemon.Breeding.Female} %
+                        </div>
+                    </div>
+
+                    <div class="Pokemon-Info">
+                        <span class="Info-Left">Grow Rate</span>
+                        <span class="title">${pokemon.Breeding.Growth_Rate}</span>
+
+
+                    </div>
+
+                    <div class="Pokemon-Info">
+                        <span class="Info-Left">Egg Groups</span>
+                        <span class="title">${pokemon.Breeding.Egg_Groups}</span>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+function calculateHeight(pokemon) {
+    let heightDM = pokemon.About.Height;
+    let getMeter = +heightDM / 10;
+    let getInchTotal = +heightDM * 10 * 0.393701;
+    let inches = getInchTotal
+    let feet = Math.floor(inches / 12);
+    inches = inches / 12;
+    inches = inches.toFixed(1)
+    return (feet + "' " + inches + '" ' + " (" + getMeter + "  meter" + ")")
+}
+
+function calculateWeight(pokemon) {
+    let weightHG = pokemon.About.Weight;
+    let weightLBS = +weightHG / 4.53592;
+    weightLBS = weightLBS.toFixed(2);
+    let weightKG = +weightHG / 10;
+    return (weightLBS + " lbs" + "  (" + weightKG + " kg" + ")")
+}
+
+
+function getActive() {
+    const categoryContainers = document.querySelectorAll('.Categorys');
+
+    categoryContainers.forEach(categoryContainer => {
+        // Event-Listener hinzufügen
+        categoryContainer.addEventListener('click', function (event) {
+            // Überprüfen, ob ein <span> angeklickt wurde
+            if (event.target.tagName === 'SPAN') {
+                // Entferne die Klasse 'active' von allen <span>-Elementen
+                const spans = categoryContainer.querySelectorAll('span');
+                spans.forEach(span => span.classList.remove('active'));
+
+                // Füge die Klasse 'active' zu dem angeklickten <span> hinzu
+                event.target.classList.add('active');
+            }
+        });
+    });
+}
+
+
+function showCorrectId(id) {
+    return id.toString().padStart(3, '0');
+}
+
+
+function insertMoves(pokemonID) {
+    let selectedPoke = [...allPoke, ...favoredPoke].find(poke => poke.Id === pokemonID);
+    if (!selectedPoke) {
+        console.error("Pokemon Id was not found!");
+        return;
+    }
+    let moves = document.getElementById('insertData');
+    moves.classList.remove('flex-row');
+
+
+
+    moves.innerHTML = "";
+    let moveslist = selectedPoke.Moves.split(' ');
+    for (let index = 0; index < moveslist.length; index++) {
+        const move = moveslist[index];
+        moves.innerHTML += `
+        <div>
+            <ul>
+                <li class="title">${move}</li>
+            </ul>
+        </div>
+        `;
+
+    }
+}
+
+
+async function fetchPokebyId(pokemonID) {
+    try {
+        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`);
+        if (!response.ok) {
+            throw new Error("Fehler beim Laden des Pokemons");
+        }
+        let data = await response.json();
+        console.log(data);
+        
+        return {
+            "Id": data.id,
+            "Name": data.name,
+            "Image": data.sprites.other.dream_world.front_default
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+
+   async function getEvolution(pokemonID) {
+        let selectedPoke = [...allPoke, ...favoredPoke].find(poke => poke.Id === pokemonID);
+        if (!selectedPoke || !selectedPoke.Evolution_Chain) {
+            console.error("Pokemon ID oder Evolution Chain wurde nicht gefunden!");
+            return;
+        }
+
+        let evolutionChain = selectedPoke.Evolution_Chain.chain;
+        if (!evolutionChain) {
+            console.error("Evolution Chain ist nicht vorhanden!");
+            return;
+        }
+
+        let content = document.getElementById('insertData')
+        content.innerHTML = "";
+        content.classList.add('flex-row');
+        let htmlContent = "";
+
+        let currentStage = evolutionChain;
+        let imageCounter = 0;
+        while (currentStage) {
+            let stageId = extractIdFromUrl(currentStage.species.url);
+            let pokeData = [...allPoke, ...favoredPoke].find(poke => poke.Id == stageId);
+            if (!pokeData) {
+                pokeData = await (fetchPokebyId(stageId));
+            }
+         
+
+            if (pokeData && pokeData.Id) {
+                if (imageCounter > 0) {
+                    htmlContent += `
+                      <img class="arrow${imageCounter}" src="./assests/doubleArrow.png">
+                    `;
+                }
+                htmlContent += `
+            <div class="evolution${imageCounter}">
+            <img src="${pokeData.Image}" class="evolution-image">
+            <p class="title small-FS">${pokeData.Name}</p>
+            </div>
+            `;
+                imageCounter++;
+            }
+            currentStage = currentStage.evolves_to[0];
+            content.innerHTML = htmlContent;
+        }
+    }
+
+
+    function extractIdFromUrl(url) {
+        let parts = url.split('/');
+        return +parts[parts.length - 2];
+    }
+
+
+    function renderPolarChart(labels, data) {
+        // Holen des Containers
+        const container = document.getElementById('insertData');
+        container.innerHTML = ''; // Vorherigen Inhalt entfernen
+
+        // Hinzufügen eines Canvas-Elements
+        const canvas = document.createElement('canvas');
+        canvas.id = 'polarChart';
+        container.appendChild(canvas);
+
+        // Chart erstellen
+        new Chart(canvas, {
+            type: 'polarArea',
+            data: {
+                labels: labels,
+                datasets: [{
+                    data: data,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                animation: {
+                    animateScale: true
+                },
+                scales: {
+                    r: {
+                        max: 100,
+                        ticks: {
+                            display: true // Zeigt Werte um den Kreis herum an
+                        },
+                        pointLabels: {
+                            display: false, // Zeigt die Labels an den Achsen an
+                            font: {
+                                size: 8 // Anpassung der Schriftgröße
+                            }
+                        }
+                    }
+                },
+                plugins: {
+                    legend: {
+                        display: true,
+                        position: 'bottom', // Legt die Legende neben dem Chart an
+                        labels: {
+                            font: {
+                                size: 12 // Anpassung der Schriftgröße für die Labels
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    }
+
